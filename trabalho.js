5724badd8421c294dae5300b33c1a90e0c64d234
SamuelKrakheckeDias <161588543+SamuelKrakheckeDias@users.noreply.github.com>
2025-03-26 08:32:02 -0300

    Add files via upload

---
+++ trabalho.js
@@ -0,0 +1,7 @@
+   let frutas = [ "Tomate", "Maça", "Abacaxi", "Amora", "Laranja", "Kiwi", "Limão", "Banana", "Abacate", "Acerola" ];
+
+   for ( indice = 0; indice <= 9; indice ++) {
+  
+   console.log ( frutas [ indice ] );
+       
+}
